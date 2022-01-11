4a8f0ca4c6e7f3d4ef2eebbc64f8a6936bc7e0d0
fahriesa02 <fujinkai02@gmail.com>
2022-01-11 16:00:14 +0800

    pid-controller file for raspi

---
+++ raspi/pid-controller.py
@@ -0,0 +1,15 @@
+# import library yang dibutuhkan
+import RPi.GPIO as GPIO
+import time
+
+# declare variabel yang digunakan
+
+
+# declare fungsi yang akan digunakan
+
+
+# do the function
+
+
+# cleanup gpio
+GPIO.cleanup()
